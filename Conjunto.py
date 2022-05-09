f02ebabf8c3a14d71ce82d3d3a0993060b661358
CandeZ <candelazumel@gmail.com>
2022-05-08 23:55:14 -0300

    ejercicio 8 terminado

--- Conjunto.py
+++ Conjunto.py
@@ -6,10 +6,10 @@ class Conjunto:
 
     def cargar(self):
         self.__lista = []
-        print("INGRESE LOS ELEMENTOS DEL CONJUNTO, PARA FINALIZAR INGRESE: FIN")
+        print("INGRESE LOS ELEMENTOS DEL CONJUNTO, PARA FINALIZAR INGRESE: 0")
         while True:
             elem = int(input("ELEMENTO: "))
-            if(elem == "FIN"):
+            if(elem == "0"):
                 break
             self.__lista.append(elem)
             self.__lista.sort()
